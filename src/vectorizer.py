3dcde8ad70c5fc0e4f54b41b0de5711f4e39ec87
Fredrik Wännman <frwa1800@student.miun.se>
2025-04-06 22:46:22 +0200

    added annotation for types to vectorizer

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,5 +1,6 @@
+from sklearn.base import BaseEstimator
 
-def vectorize_data(vectorizer, X_train, X_test, fit=True):
+def vectorize_data(vectorizer: BaseEstimator,  X_train: list, X_test:list, fit: bool=True) -> tuple:
     
     if fit:
         X_train_vect = vectorizer.fit_transform(X_train)
