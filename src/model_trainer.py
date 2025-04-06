f84ea0e0a86a553ca4ad56e16bd35e9ab38f2b32
Fredrik Wännman <frwa1800@student.miun.se>
2025-04-06 22:27:15 +0200

    implemented model trainer

---
+++ src/model_trainer.py
@@ -0,0 +1,7 @@
+from sklearn.base import BaseEstimator
+import numpy as np
+
+def train_model(model: BaseEstimator, X: np.ndarray, y:np.ndarray) -> BaseEstimator:
+    
+    model.fit(X, y)
+    return model
