1c2d108d14c84fd9f8cdb97f414b3b36443f1d7d
Fredrik Wännman <frwa1800@student.miun.se>
2025-04-10 12:50:31 +0200

    added textattack to perturbation

--- src/perturbation.py
+++ src/perturbation.py
@@ -1,9 +1,19 @@
-
+from typing import Optional
 from numpy import ndarray
+from textattack.augmentation import Augmenter, EasyDataAugmenter
+
+def apply_perturbation(X: ndarray, level: float = 0.2, augmenter: Optional[Augmenter] = None) -> list[str]:
+    
+    if augmenter is None:
+        augmenter = EasyDataAugmenter(pct_words_to_swap=level)
+
+    perturbed_data = []
+
+    for text in X:
+        augmented = augmenter.augment(text)
+        perturbed_data.append(augmented[0] if augmented else text)
+
+    return perturbed_data
 
 
-def apply_perturbation(X: ndarray, level: int) -> list:
 
-    # Do stuff
-
-    return []
