4dda72bcaedcd60f3626c64e698f2924771b2832
Fredrik Wännman <frwa1800@student.miun.se>
2025-04-13 21:25:54 +0200

    splited vectorizer in two methods for more modularity

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,14 +1,12 @@
+import numpy as np
 from sklearn.base import BaseEstimator
 
-def vectorize_data(vectorizer: BaseEstimator,  X_train: list, X_test:list, fit: bool=True) -> tuple:
-    
-    if fit:
-        X_train_vect = vectorizer.fit_transform(X_train)
-        X_test_vect = vectorizer.transform(X_test)
+# Use for training data
+def vectorize_data_fit(vectorizer: BaseEstimator, X_train: np.ndarray) -> np.ndarray:
+    X_vect = vectorizer.fit_transform(X_train)
+    return X_vect
 
-    else:
-        X_train_vect = vectorizer.transform(X_train)
-        X_test_vect = vectorizer.transform(X_test)
-    
-    return X_train_vect, X_test_vect
-
+# Use for testing data
+def vectorize_data(vectorizer: BaseEstimator, X_test: np.ndarray) -> np.ndarray:
+    X_vect = vectorizer.transform(X_test)
+    return X_vect
