66416515b1b421a9e4f1b35c8f63927d4f2dabd5
Fredrik Wännman <frwa1800@student.miun.se>
2025-04-13 21:27:29 +0200

    added combined perturbation

--- src/perturbation.py
+++ src/perturbation.py
@@ -1,14 +1,21 @@
+import random
 from numpy import ndarray
-from textattack.augmentation import EasyDataAugmenter
+from textattack.augmentation import WordNetAugmenter, EasyDataAugmenter, CharSwapAugmenter
 
 def apply_perturbation(X: ndarray, level: float = 0.3) -> list[str]:
-    
-    augmenter = EasyDataAugmenter(pct_words_to_swap=level)
+    random.seed(42)  # Set a seed for reproducibility
+
+    wordnet_augmenter = WordNetAugmenter(pct_words_to_swap=level, transformations_per_example=1)
+    charswap_augmenter = CharSwapAugmenter(pct_words_to_swap=level, transformations_per_example=1)
+
     perturbed_data = []
 
     for text in X:
-        augmented = augmenter.augment(text)
-        perturbed_data.append(augmented[0] if augmented else text)
+        augmented = wordnet_augmenter.augment(text)
+        augmented_text = augmented[0] if augmented else text
+
+        augmented = charswap_augmenter.augment(augmented_text)
+        perturbed_data.append(augmented[0] if augmented else augmented_text)
 
     return perturbed_data
 
