917fe26d537bd9bcd6d7832a1858a7c9e464b708
Fredrik Wännman <frwa1800@student.miun.se>
2025-05-21 21:29:46 +0200

    refactored perturbation

--- src/perturbation.py
+++ src/perturbation.py
@@ -8,6 +8,7 @@ from textattack.augmentation import WordNetAugmenter, CharSwapAugmenter, Deletio
 def apply_perturbation(
     X: np.ndarray,
     level: float = 0.3,
+    augmentation_type: str = "wordnet",
     save_path: Optional[str] = None,
     load_path: Optional[str] = None
 ) -> np.ndarray:
@@ -20,17 +21,27 @@ def apply_perturbation(
     # Otherwise, perform perturbation
     pct_words_to_swap = 1.0
     random.seed(42)
-    
-    augmenter = WordNetAugmenter(pct_words_to_swap=pct_words_to_swap, transformations_per_example=1)
-    #augmenter = CharSwapAugmenter(pct_words_to_swap=pct_words_to_swap, transformations_per_example=1)
-    #augmenter = DeletionAugmenter(pct_words_to_swap=0.7, transformations_per_example=1)
 
+    # Select augmenter
+    if augmentation_type == "wordnet":
+        augmenter = WordNetAugmenter(pct_words_to_swap=pct_words_to_swap, transformations_per_example=1)
+    elif augmentation_type == "charswap":
+        augmenter = CharSwapAugmenter(pct_words_to_swap=pct_words_to_swap, transformations_per_example=1)
+    elif augmentation_type == "deletion":
+        augmenter = DeletionAugmenter(pct_words_to_swap=0.7, transformations_per_example=1)
+    else:
+        raise ValueError(f"Unsupported augmentation type: {augmentation_type}")
+    
+
+    # Calulate number and randomize lines to augment
     num_lines = len(X)
     num_to_augment = int(num_lines * level)
     data_to_augment = random.sample(range(num_lines), num_to_augment)
 
+    # Create a copy of the original data
     perturbed_X = X.copy()
 
+    # Apply augmentation
     for idx in data_to_augment:
         perturbed_X[idx] = augmenter.augment(X[idx])[0]
 
